7a71094de09d1f539b5796b3e5be2f1ca5afbd0b
changhoi <0417kch@naver.com>
2019-07-31 10:01:54 +0900

    check how to use styled-components in typescript

--- src/compoenents/backgroundDiv.tsx
+++ src/compoenents/backgroundDiv.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import * as styled from "@types/styled-components"; // yarn add @types/styled-components;
 
 interface IBgDiv {
   bgColor: String;
---
+++ src/compoenents/headerComponents.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const Header: React.FC = () => (
+  <div className="header__wrapper">
+    <div className="header__division">Drama</div>
+    <div className="header__division">Movie</div>
+    <div className="header__division">Search</div>
+  </div>
+);
